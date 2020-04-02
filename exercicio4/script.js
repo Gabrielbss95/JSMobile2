4c955fa55ed2f80168bec274785e83b2f8f7fe23
Gabriel Barbosa <gabrielbss9@gmail.com>
2020-04-01 22:41:31 -0300

    Update exe 5

--- exercicio4/script.js
+++ exercicio4/script.js
@@ -9,6 +9,23 @@ const escrevaNumeros = (inicio, fim) => {
     }, 1000);
 }
 
+const escrevaNumeros2 = (inicio, fim) => {
+
+    const numeros2 = document.getElementById("numeros2");
+
+    let timerId = setTimeout(tick = () => {
+
+        if(inicio<=fim){
+            numeros2.innerHTML+= " " + inicio++;
+        } else clearInterval(intervalId);
+
+        timerId = setTimeout(tick, 1000);
+    }, 1000);
+
+}
+
+
 window.onload = () => {
-    escrevaNumeros(1,9);
+    escrevaNumeros(1, 9);
+    escrevaNumeros2(1, 9);
 }
